7a3f9d7a0d55f47a26826d9f227dbadd647df708
Marni Hager <mlh6118@gmail.com>
2022-01-19 21:32:32 -0800

    Added access to Mongo.

--- server.js
+++ server.js
@@ -3,11 +3,21 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+// const Book = require('./models/bookModel');
 
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
+
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Mongoose is connected')
+});
 
 app.get('/test', (request, response) => {
 
