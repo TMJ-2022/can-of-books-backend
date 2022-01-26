51bc279cf8056a3309036ddd80e6cbec7cb7aa8e
Tiara Brown <tiaradominique@gmail.com>
2022-01-26 00:47:29 -0500

    Started routing for delete

--- server.js
+++ server.js
@@ -3,32 +3,35 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
-const Book = require('./models/bookModel');
 const seed = require('./seed.js');
-
 const app = express();
 app.use(cors());
+app.use(express.json());
+
+const mongoose = require('mongoose');
+const Book = require('./models/bookModel');
 
 const PORT = process.env.PORT || 3002;
 
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(console.log('Mongoose is connected')).then(seed());
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(console.log('Mongoose is connected')).then(seed());
 
 app.get('/', (request, response) => {
   response.send('test request received')
 })
 
 app.get('/books', handleGetBooks);
+app.post('/books', handleNewBook);
+app.delete('/books/:id', handleRemoveBook);
 
 async function handleGetBooks(request, response) {
-  console.log("request", request.body);
-  console.log("response", response.body);
+  // console.log("request", request.body);
+  // console.log("response", response.body);
   try {
-      let queryObj = {};
-      if (request.query.email) {
-        queryObj = {email: request.query.email}
-      }
-      let booksFromDB = await Book.find(queryObj);
+    let queryObj = {};
+    if (request.query.email) {
+      queryObj = { email: request.query.email }
+    }
+    let booksFromDB = await Book.find(queryObj);
 
     if (booksFromDB) {
       response.status(200).send(booksFromDB);
@@ -40,5 +43,38 @@ async function handleGetBooks(request, response) {
     response.status(500).send('Server error.');
   }
 }
+
+async function handleNewBook(request, response) {
+  try {
+    const book = await Book.create(request.body);
+    response.send(book);
+  } catch (error) {
+    console.error(error);
+    response.status(400).send('Could not create book');
+  }
+}
+
+async function handleRemoveBook(request, response) {
+  try {
+    const email = request.query.email;
+
+    const id = request.params.id;
+
+    const book = await Book.findOne({ _id: id, email: email });
+
+    if (!book) {
+      response.status(400).send('Failed to remove book');
+      return;
+    }
+
+    await Book.findByIdAndDelete(id);
+
+    response.send('success');
+  } catch (error) {
+    console.error(error);
+    response.status(400).send('Failed to remove book')
+  }
+}
+
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
